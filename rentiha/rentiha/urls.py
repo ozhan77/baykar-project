dbe7009ed6f8d1250056e22c486248ad4ed8ab6f
oguzhan daskaya <oguzhandky@gmail.com>
2023-09-16 20:59:28 +0300

    login register logout

--- rentiha/rentiha/urls.py
+++ rentiha/rentiha/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from rentlist import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.SignupPage,name='signup'),
+    path('login/',views.LoginPage,name='login'),
+    path('home/',views.HomePage,name='home'),
+    path('logout/',views.LogoutPage,name='logout'),
 ]
