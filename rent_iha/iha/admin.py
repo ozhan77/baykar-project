5e59f11a1dee0629ab79bd8103df53083c906f8e
oguzhan daskaya <oguzhandky@gmail.com>
2023-09-18 21:18:09 +0300

    add api

--- rent_iha/iha/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import iha,Category
-# Register your models here.
-admin.site.register(iha)
-admin.site.register(Category)
--- rent_iha/iha/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class IhaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'iha'
