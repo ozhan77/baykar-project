a785a713584bd5d09319b74fe5956e2b73348e9a
oguzhan daskaya <oguzhandky@gmail.com>
2023-09-18 21:48:27 +0300

    add catalog

--- commerce/auctions/forms.py
+++ commerce/auctions/forms.py
@@ -5,10 +5,11 @@ from .models import User,Category,Listing,Logs
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Listing
-        fields=['brand','model','description','owner']
+        fields=['brand','model','description','imageUrl','owner']
         widgets = {
             'brand': forms.TextInput(attrs={'class':'form-control'}),
             'model': forms.TextInput(attrs={'class':'form-control'}),
             'description': forms.TextInput(attrs={'class':'form-control'}),
+            'imageUrl': forms.TextInput(attrs={'class':'form-control'}),
             'owner': forms.HiddenInput(),
         }
