e183e937c493f347d63ab6204f43faa723fd284b
oguzhan daskaya <oguzhandky@gmail.com>
2023-09-18 21:50:17 +0300

    final

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "xtpghulp",
+        "USER": "xtpghulp",
+        "PASSWORD": "zSRTWjyq_okqwCSrnfWlwWYjfEvyUMAT",
+        "HOST": "rain.db.elephantsql.com",
+        "PORT": "5432",
     }
 }
 
